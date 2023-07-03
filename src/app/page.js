2fd70d08f9f14f083b153e68d91b5404d1d3158a
Erick <128732040+Erick-Iuri@users.noreply.github.com>
2023-07-03 20:04:11 -0300

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import { Flex, Text } from "@chakra-ui/react"
+
+
+export default function Home() {
+  return (
+   <Flex justify={"center"}>
+      <Flex margin={"100px"}>
+        <Text bg="blue" w="100px" h="100px"></Text>
+      </Flex>
+   </Flex>
+  )
+}
